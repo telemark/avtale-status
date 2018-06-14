0172ece5364932f38f7c2c3457157e26ea59bcb2
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-06-14 10:52:32 +0200

    Fixes isExpired issue

--- test/lib/generate-assignement-status-test.js
+++ test/lib/generate-assignement-status-test.js
@@ -5,3 +5,8 @@ test('it returns signed for LEST if no signature required', t => {
   const data = require('../data/no-signature-read.json')
   t.deepEqual(generateStatus(data), 'signed', 'signed OK')
 })
+
+test('it returns expired for SENDT_SDP if no signature required and date expired', t => {
+  const data = require('../data/no-signature-expired.json')
+  t.deepEqual(generateStatus(data), 'expired', 'expired OK')
+})
