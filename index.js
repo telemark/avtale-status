469fa7213a4fffeb4af2b20b578c1ffd9f21f07d
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-08-09 14:57:22 +0200

    Changes to continous writing of jobs (minor)

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const getUnsignedAgreements = require('./lib/steps/get-unsigned-agreements')
 const checkAgreementsStatus = require('./lib/steps/check-agreements-status')
-const generateUpdateJobs = require('./lib/steps/generate-update-jobs')
-const saveJobs = require('./lib/steps/save-jobs')
 const logger = require('./lib/logger')
 
 logger('info', ['index', 'start'])
 
 getUnsignedAgreements()
   .then(checkAgreementsStatus)
-  .then(generateUpdateJobs)
-  .then(saveJobs)
   .then(data => {
     logger('info', ['index', data._id, 'finished'])
     process.exit(0)
