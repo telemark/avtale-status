834b5e14bb9554e80e9a53eeb14c0d2b031ea603
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-08-31 12:26:04 +0200

    Adds posting of stats at the end (minor)

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const getUnsignedAgreements = require('./lib/steps/get-unsigned-agreements')
 const checkAgreementsStatus = require('./lib/steps/check-agreements-status')
+const updateStatusStats = require('./lib/steps/update-status-stats')
 const logger = require('./lib/logger')
 
 logger('info', ['index', 'start'])
 
 getUnsignedAgreements()
   .then(checkAgreementsStatus)
+  .then(updateStatusStats)
   .then(data => {
     logger('info', ['index', data._id, 'finished'])
     process.exit(0)
